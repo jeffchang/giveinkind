33921390c3964dea1561a740434ae9b4a9573a71
Jeff Chang <jeffchang.ca@gmail.com>
2013-10-04 22:47:48 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giveinkind::Application.config.secret_key_base = '26eef467c6792be8f2b7fc3e371715d64b74cc268e8f2bdfaeca8dd4565fe3efd0c83d872f2ce049cfa2fb6aa529250f959ac3fea7628126665c92632e4dddd1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Giveinkind::Application.config.session_store :cookie_store, key: '_giveinkind_session'
